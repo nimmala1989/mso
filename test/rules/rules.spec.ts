e2ec1978532882b32f28122254e4d34327a3a039
nimmala1989 <82732221+nimmala1989@users.noreply.github.com>
2021-10-25 05:37:51 -0400

    added spec file

--- test/rules/rules.spec.ts
+++ test/rules/rules.spec.ts
@@ -7,11 +7,12 @@ import { EditOrView } from '../../pages/rules/editOrView.po';
 import { Table } from '../../pages/rules/table.po';
 
 test.describe("On Rules Page", async () => {
-    let login: Login 
+    let login: Login
     let rules: Rules
     let create: Create
     let editOrView: EditOrView
     let table: Table
+    let authorizationToken: string
 
     test.beforeEach(async ({ page }) => {
         login = new Login(page);
@@ -19,95 +20,103 @@ test.describe("On Rules Page", async () => {
         create = new Create(page);
         editOrView = new EditOrView(page);
         table = new Table(page);
-
-        await page.goto(pageUrl, { timeout: 120000, waitUntil: 'load' });
+        page.on('request', request => {
+            let allHeaders = request.headers()
+            if (allHeaders.authorization && allHeaders.authorization != 'Bearer null') {
+                authorizationToken = allHeaders.authorization
+            }
+        })
+        const ad = await page.goto(pageUrl, { timeout: 120000, waitUntil: 'load' });
         await login.loginToTheApplication('qauser1', 'monozukuri')
         await login.selectClient('FAB2')
         await rules.waitForPageLoad()
     })
 
-    test("Create a percent rule with mandatory fields and verify", async ({ page }) => {
-        await rules.openRulesPopup()
-        await create.percentageRule()
-        await table.selectByName(create.data.name)
-        await page.waitForSelector('mat-drawer[mode="side"]')
-        await editOrView.verifyName(create.data.name)
-    })
-
-    test("Create a event rule with mandatory fields and verify", async ({ page }) => {
-        await rules.openRulesPopup()
-        await create.eventRule()
-        await table.selectByName(create.data.name)
-        await page.waitForSelector('mat-drawer[mode="side"]')
-        await editOrView.verifyName(create.data.name)
-    })
-
-    test("Create a time rule with mandatory fields and verify", async ({ page }) => {
-        await rules.openRulesPopup()
-        await create.timeRule()
-        await table.selectByName(create.data.name)
-        await page.waitForSelector('mat-drawer[mode="side"]')
-        await editOrView.verifyName(create.data.name)
-    })
-
-    test("Check that rules with duplicate name cannot be created", async ({ page }) => {
-        await rules.openRulesPopup()
-        await create.percentageRule()
-        await table.selectByName(create.data.name)
-        await page.waitForSelector('mat-drawer[mode="side"]')
-        await rules.openRulesPopup()
-        await create.verifyDuplicateRuleErrorMessage(create.data.name)
-    })
-
-    test("Check that error displayed with wrong percentage", async ({ page }) => {
-        await rules.openRulesPopup()
-        await create.tryToCreateRuleWithWrongPercentageAndVerifyError()
-    })
-
-    test.describe('For Advance Settings', async () => {
-
-        test('Verify Maximum Cannot be greater than lot error', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.advanceSettingsErrorMessages();
-            errorMessages.verifyMaximumCannotBeGreaterThanLot()
+    test.describe('Create a new rule', async () => {
+        test("Create a percent rule with mandatory fields and verify it is created", async () => {
+            await rules.openRulesPopup()
+            await create.percentageRule()
+            await table.selectByName(create.data.name)
+            await editOrView.verifyName(create.data.name)
         })
 
-        test('Verify minimum Cannot be greater than lot error', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.advanceSettingsErrorMessages();
-            errorMessages.verifyMinimumCannotBeGreaterThanLot()
+        test("Create a event rule with mandatory fields and verify it is created", async () => {
+            await rules.openRulesPopup()
+            await create.eventRule()
+            await table.selectByName(create.data.name)
+            await editOrView.verifyName(create.data.name)
         })
 
-        test('Verify minimum cannot be greater than maximum', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.advanceSettingsErrorMessages();
-            errorMessages.verifyMinimumCannotBeGreaterThanMaximum()
+        test("Create a time rule with mandatory fields and verify it is created", async () => {
+            await rules.openRulesPopup()
+            await create.timeRule()
+            await table.selectByName(create.data.name)
+            await editOrView.verifyName(create.data.name)
         })
 
-        test('Verify percentage must be between min and max', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.advanceSettingsErrorMessages();
-            errorMessages.verifyPercentageMustBeBetweenMinAndMax()
+        test("Create a rule including process links and verify it is created", async () => {
+            await rules.openRulesPopup()
+            await create.processLink()
+            await table.selectByName(create.data.name)
+            await editOrView.verifyName(create.data.name)
+
         })
     })
 
-    test.describe('For Expiration field', async () => {
+    test.describe('For Negative scenarios', async () => {
+        test("Check that rules with duplicate name cannot be created", async () => {
+            await rules.openRulesPopup()
+            await create.percentageRule()
+            await table.selectByName(create.data.name)
+            await rules.openRulesPopup()
+            await create.verifyDuplicateRuleErrorMessage(create.data.name)
+        })
 
-        test('Verify Expiration date cannot be empty', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.expirationErrorMessages();
-            errorMessages.verifyEmptyExpireDateErrorMessage()
+        test("Check that error displayed with wrong percentage", async () => {
+            await rules.openRulesPopup()
+            await create.tryToCreateRuleWithWrongPercentageAndVerifyError()
         })
-        test('Verify Warning and expiration dates cannot be same', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.expirationErrorMessages();
-            errorMessages.verifyWarningAndExpirationDateCannotBeSame()
+
+        test.describe('For Advance Settings', async () => {
+
+            test('Verify different error messages', async ({ page }) => {
+                await rules.openRulesPopup();
+                const errorMessages = await create.advanceSettingsErrorMessages();
+                await errorMessages.verifyMaximumCannotBeGreaterThanLot()
+                await errorMessages.verifyMinimumCannotBeGreaterThanLot()
+                await errorMessages.verifyMinimumCannotBeGreaterThanMaximum()
+                await errorMessages.verifyPercentageMustBeBetweenMinAndMax()
+            })
         })
-        test('Verify Warning date must be before expiration date', async ({page}) => {
-            await rules.openRulesPopup();
-            const errorMessages = await create.expirationErrorMessages();
-            errorMessages.verifyWarningDateMustBeBeforeExpirationDate()
+
+        test.describe('For Expiration field', async () => {
+
+            test('Verify Expiration date cannot be empty', async () => {
+                await rules.openRulesPopup();
+                const errorMessages = await create.expirationErrorMessages();
+                await errorMessages.verifyEmptyExpireDateErrorMessage()
+                await errorMessages.verifyWarningAndExpirationDateCannotBeSame()
+                await errorMessages.verifyWarningDateMustBeBeforeExpirationDate()
+            })
         })
+    })
+
+    test.describe('Update an existing rule', async () => {
+        test('Update rule name and verify', async () => {
+            await rules.openRulesPopup()
+            await create.percentageRule()
+            await table.selectByName(create.data.name)
+            await editOrView.verifyName(create.data.name)
+            const newName = await editOrView.updateName()
+            await editOrView.clickSave()
+            await editOrView.commentAndSave()
+            await table.selectByName(newName)
+            await editOrView.verifyName(newName)
+        })
+    })
+
+    test.afterAll(async () => {
+        await editOrView.deleteRulesCreateByAutomation(authorizationToken)
     })
 })
 
